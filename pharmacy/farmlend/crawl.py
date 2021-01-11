4730d22008edeaf91e800c1d91361c748b3ec8cb
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2021-01-11 23:00:55 +0500

    Update crawl.py

--- pharmacy/farmlend/crawl.py
+++ pharmacy/farmlend/crawl.py
@@ -12,6 +12,7 @@ result_path = 'result/%s' % datetime.today().date()
 
 Path(result_path).mkdir(parents=True, exist_ok=True)
 
+
 def parse_product(city, id):
     html = requests.get('%s/%s/product/%d' % (host, city, id))
     product = BeautifulSoup(html.text, features='html.parser')
@@ -31,18 +32,38 @@ def parse_product(city, id):
     if value == None:
         return res
 
-    prices = [item['data-price'] for item in price_block.find_all('div', attrs={'data-price' : True})]
+    prices = [item['data-price'] for item in price_block.find_all('div', attrs={'data-price': True})]
     min_price = min(prices)
     max_price = max(prices)
 
-    return res.append(pd.DataFrame([[city, title.text, min_price, max_price]], columns = ['city', 'title', 'price_min', 'price_max']), ignore_index=True)
+    return res.append(
+        pd.DataFrame([[city, title.text, min_price, max_price]], columns=['city', 'title', 'price_min', 'price_max']),
+        ignore_index=True)
+
 
 cities = [
-    'orenburg', 'samara', 'ekaterinburg', 'tyumen', 'chelyabinsk',
-    'ufa', 'kazan' 'izhevsk',
-    'orsk', 'buzuluk', 'neftekamsk', 'sterlitamak', 'bugulma',
-    'tyumen', 'naberezhnye-chelny', 'sarapul', 'votkinsk',
-    'tolyatti', 'nizhnii-tagil', 'verhnyaya-salda', 'zlatoust', 'magnitogorsk'
+    # 'orenburg',
+    # 'samara',
+    # 'ekaterinburg',
+    # 'tyumen',
+    # 'chelyabinsk',
+    # 'ufa',
+    # 'kazan'
+    # 'izhevsk',
+    'orsk',
+    'buzuluk',
+    # 'neftekamsk',
+    # 'sterlitamak',
+    # 'bugulma',
+    # 'tyumen',
+    # 'naberezhnye-chelny',
+    # 'sarapul',
+    # 'votkinsk',
+    # 'tolyatti',
+    # 'nizhnii-tagil',
+    # 'verhnyaya-salda',
+    # 'zlatoust',
+    # 'magnitogorsk'
 ]
 
 results = {}
@@ -67,6 +88,5 @@ for id in range(0, 40500):
         time.sleep(.3)
 
     with open('result/time.txt', 'a') as file:
-    	file.write('%d crawl time %f\n' % (id, time.time() - start))
-    	file.close()
-
+        file.write('%d crawl time %f\n' % (id, time.time() - start))
+        file.close()
